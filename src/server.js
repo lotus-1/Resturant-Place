18e25b74d35ad139518d4d77cec4431246e977aa
aman halabyar <amanhalabyar7@gmail.com>
2019-06-02 13:39:39 +0300

    addung logout file

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const router = require('./router');
-
-const server = http.createServer(router);
-const port = process.env.PORT || 5000;
-
-server.listen(port, () => {
-  console.log(`Magic happens on http://localhost:${port}`);
-});
